adb833f49db20fb12dce385dfd0b72cd5ea2d119
moxdev <moxdev1@gmail.com>
2017-12-13 11:37:04 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# TidBitTuts Blog
---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    title: `TidBitTuts`
+  },
+  plugins: [
+    // Specify Filesystem Source
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src`
+      }
+    },
+    // Markdown Parsing
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: false,
+              // Analyze images' pixel density to make decisions about
+              // target image size. This is what GitHub is doing when
+              // embedding images in tickets. This is a useful setting
+              // for documentation pages with a lot of screenshots.
+              // It can have unintended side effects on high pixel
+              // density artworks.
+              //
+              // Example: A screenshot made on a retina screen with a
+              // resolution of 144 (e.g. Macbook) and a width of 100px,
+              // will be rendered at 50px.
+              //
+              // Defaults to false.
+              sizeByPixelDensity: true
+            }
+          }
+        ]
+      }
+    },
+    // Setup Google Fonts
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ]
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`
+  ]
+};
